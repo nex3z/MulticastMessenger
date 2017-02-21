4e31cb70197a72e83375e52bd64d354023d492ad
nex3z <litianxing9@gmail.com>
2017-02-21 21:46:31 +0800

    Remove static getter for application context.

--- app/src/main/java/com/example/nex3z/multicastmessenger/app/App.java
+++ app/src/main/java/com/example/nex3z/multicastmessenger/app/App.java
@@ -1,25 +1,18 @@
 package com.example.nex3z.multicastmessenger.app;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.example.nex3z.multicastmessenger.internal.dagger.component.AppComponent;
 import com.example.nex3z.multicastmessenger.internal.dagger.component.DaggerAppComponent;
 import com.example.nex3z.multicastmessenger.internal.dagger.module.AppModule;
 
 public class App extends Application {
-    private static Context mContext;
     private AppComponent mAppComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mContext = getApplicationContext();
         initInjector();
-    }
-
-    public static Context getAppContext() {
-        return mContext;
     }
 
     private void initInjector() {
@@ -31,6 +24,5 @@ public class App extends Application {
     public AppComponent getApplicationComponent() {
         return mAppComponent;
     }
-
 
 }
